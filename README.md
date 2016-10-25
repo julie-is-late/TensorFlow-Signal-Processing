cc595fcb50985b9d690379312a8ad237edb6a1e1
Julie Shapiro <jshap70@users.noreply.github.com>
2016-10-25 17:07:43 -0400

    export data and clean up scripts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DeepSP
 doing audio digital signal processing in tensorflow
+
+
+
+Sources:
+    audio made by self
+    some midi from - http://www.piano-midi.de/brahms.htm
+    some scripts from - https://github.com/mgeier/python-audio/
+    wavio - Warren Weckesser
--- convert_data.py
+++ convert_data.py
@@ -1,31 +1,55 @@
+#!/usr/bin/env python
+"""This script converts wav files from 8, 16, or 24 bit PCM(Integer) to np.float32 np.arrays
+    and saves them to the right locations"""
+
 import os
 
 import numpy as np
 
 import modules.wavio as wavio
 
-FILENAME = './sound_files/lowpass - post - plysaw.wav'
-OUT_DIR = './data'
 
-inn = wavio.read(FILENAME)
+def export_data(directory, filename):
+    """Converts wav files from 8, 16, or 24 bit PCM(Integer) to np.float32 np.arrays and saves them to the right locations based on filename.
 
-# convert to float and normalize if needed
-# help from https://github.com/mgeier/python-audio/blob/master/audio-files/utility.py
-if inn.data.dtype.kind in 'iu':
-    i = np.iinfo(inn.data.dtype)
-    abs_max = 2 ** (i.bits - 1)
-    offset = i.min + abs_max
-    floats = (inn.data.astype(np.float32) - offset) / abs_max
+    Args:
+        directory (str): relative directory the file is in.
+        filename (str): must be of the format "<filter> - <pre/post> - <sound name>.wav".
 
-split = FILENAME.split("/")[2].split(" - ")
+    Returns:
+        nothing
+    """
 
-filter_dir = split[0]
-pre_post = split[1]
-save_file_name = split[2].split(".")[0]
+    OUT_DIR = './data'
 
-if not os.path.exists(os.path.join(OUT_DIR, filter_dir)):
-    os.makedirs(os.path.join(OUT_DIR, filter_dir))
-if not os.path.exists(os.path.join(OUT_DIR, filter_dir, pre_post)):
-    os.makedirs(os.path.join(OUT_DIR, filter_dir, pre_post))
+    inn = wavio.read(os.path.join(directory, filename))
 
-np.savez_compressed(os.path.join(OUT_DIR, filter_dir, pre_post, save_file_name), data=np.swapaxes(floats,0,1))
+    # convert to float and normalize if needed
+    # help from https://github.com/mgeier/python-audio/blob/master/audio-files/utility.py
+    if inn.data.dtype.kind in 'iu':
+        i = np.iinfo(inn.data.dtype)
+        abs_max = 2 ** (i.bits - 1)
+        offset = i.min + abs_max
+        floats = (inn.data.astype(np.float32) - offset) / abs_max
+
+    split = filename.split(" - ")
+
+    filter_dir = split[0]
+    pre_post = split[1]
+    save_file_name = split[2].split(".")[0]
+
+    if not os.path.exists(os.path.join(OUT_DIR, filter_dir)):
+        os.makedirs(os.path.join(OUT_DIR, filter_dir))
+    if not os.path.exists(os.path.join(OUT_DIR, filter_dir, pre_post)):
+        os.makedirs(os.path.join(OUT_DIR, filter_dir, pre_post))
+
+    np.savez_compressed(os.path.join(OUT_DIR, filter_dir, pre_post, save_file_name), data=np.swapaxes(floats,0,1))
+
+
+def export_filtered_audio(directory):
+    """Exports audio used as our data"""
+    # DIRECTORY = './sound_files'
+
+    for file in os.listdir(directory):
+        if file.endswith(".wav"):
+            export_data(directory, file)
