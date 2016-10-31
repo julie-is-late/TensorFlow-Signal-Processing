36bf2fa78af70407bfc0ecdf4b46f4248e86dc45
Julie Shapiro <jshap70@users.noreply.github.com>
2016-10-31 11:52:53 -0400

    actually add output to batches, derp

--- audio_batch.py
+++ audio_batch.py
@@ -1,22 +1,27 @@
 import numpy as np
 
 
-def make_batch(audio, n, batch_length, sample_offset):
+def make_batch(audio_in, audio_out, n, batch_length, sample_offset):
     """This is a tad more complicated than normal b/c batches can overlap"""
-    if n * sample_offset > audio.shape[1] - batch_length:
-        raise ValueError('too many batches %f %f' % (n * sample_offset, audio.shape[1] - batch_length))
+    if n * sample_offset > audio_in.shape[1] - batch_length:
+        raise ValueError('too many batches %f %f' % (n * sample_offset, audio_in.shape[1] - batch_length))
+
+    if audio_out.shape[1] != audio_in.shape[1]:
+        raise ValueError('audio in and audio out are not the same length')
 
     # perm = np.arange(n)
-    out = np.zeros([n, 2, batch_length])
+    input_set = np.zeros([n, 2, batch_length])
+    output_set = np.zeros([n, 2, batch_length])
 
     offset = 0
     for i in range(n):
-        out[i] = audio[:, offset:offset + batch_length]
+        input_set[i]  = audio_in[:, offset:offset + batch_length]
+        output_set[i] = audio_out[:, offset:offset + batch_length]
         offset += sample_offset
 
-    return out
+    return input_set, output_set
 
-def batch_audio(audio, seconds, valid_percent, offset=None):
+def batch_audio(audio_in, audio_out, seconds, valid_percent, offset=None):
     """Automatically batch the audio into sections of length `seconds`
 
     returns batch set and validation set"""
@@ -33,17 +38,22 @@ def batch_audio(audio, seconds, valid_percent, offset=None):
     sample_offset = int(44100 * offset)
 
 
-    ### Cut out long periods of blank
-    start = None
-    # seems space efficient /s
-    input_set = np.copy(audio)
-    for i in range(audio.shape[1]):
-        if (audio[0,i] == 0) and (audio[1,i] == 0):
-            if start is None:
-                start = i
-        else:
-            if start is not None and start - i > 1:
-                input_set = np.append(input_set[:start], input_set[i:])
+    input_set = np.copy(audio_in)
+    output_set = np.copy(audio_out)
+
+
+    # ### Cut out long periods of blank
+    # FIXME: this has bugs b/c `input_set[:start]` doesn't fix the offset of what's removed
+    # start = None
+    # # seems space efficient /s
+    # for i in range(audio_in.shape[1]):
+    #     if (audio_in[0,i] == 0) and (audio_in[1,i] == 0) and (audio_out[0,i] == 0) and (audio_out[1,i] == 0):
+    #         if start is None:
+    #             start = i
+    #     else:
+    #         if start is not None and start - i > 1:
+    #             input_set = np.append(input_set[:start], input_set[i:])
+    #             output_set = np.append(output_set[:start], output_set[i:])
 
     ### split off testing set
     num_descrete = int((input_set.shape[1] - sample_length) / sample_length)
@@ -56,13 +66,21 @@ def batch_audio(audio, seconds, valid_percent, offset=None):
     validation_pos = int((input_set.shape[1] - sample_length * num_valid) / sample_length)
     ix = np.random.permutation(validation_pos)
 
-    valid_set = audio[:, int(ix[0] * sample_length) : int(ix[0] * sample_length + num_valid * sample_length)]
-    audio = np.concatenate(
-        [ audio[:, 0: :int(ix[0] * sample_length)],
-          audio[:, int(ix[0] * sample_length + num_valid * sample_length):] ],
+    valid_set = np.asarray(
+        [ input_set[:, int(ix[0] * sample_length) : int(ix[0] * sample_length + num_valid * sample_length)],
+          output_set[:, int(ix[0] * sample_length) : int(ix[0] * sample_length + num_valid * sample_length)] ])
+    input_set = np.concatenate(
+        [ input_set[:, :int(ix[0] * sample_length)],
+          input_set[:, int(ix[0] * sample_length + num_valid * sample_length):] ],
+        axis=1)
+    output_set = np.concatenate(
+        [ output_set[:, :int(ix[0] * sample_length)],
+          output_set[:, int(ix[0] * sample_length + num_valid * sample_length):] ],
         axis=1)
 
     ### calculate number of slices
     n = int((input_set.shape[1] - sample_length) / sample_offset)
 
-    return make_batch(input_set, n, sample_length, sample_offset), valid_set
+    train_in, train_out = make_batch(input_set, output_set, n, sample_length, sample_offset)
+
+    return train_in, train_out, valid_set[0], valid_set[1]
